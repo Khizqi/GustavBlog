f9400d9b00568e9b444e2e66dddf38cdecb13f02
Khizqi <134023570+Khizqi@users.noreply.github.com>
2023-08-01 21:51:37 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -22,3 +22,56 @@ arrowIcons.forEach((icon) => {
     main1.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
   });
 });
+
+// Rotate
+
+const rotate1 = document.getElementById("rotate1");
+const rotate2 = document.getElementById("rotate2");
+const rotate3 = document.getElementById("rotate3");
+const rotate4 = document.getElementById("rotate4");
+const rotate5 = document.getElementById("rotate5");
+const photo = document.getElementById("photo");
+
+rotate1.addEventListener("click", () => {
+  photo.classList.add("animate__animated", "animate__flash");
+  photo.addEventListener("animationend", () => {
+    photo.classList.remove("animate__animated", "animate__flash");
+  });
+});
+
+rotate2.addEventListener("click", () => {
+  photo.classList.add("animate__animated", "animate__pulse");
+  photo.addEventListener("animationend", () => {
+    photo.classList.remove("animate__animated", "animate__pulse");
+  });
+});
+
+rotate3.addEventListener("click", () => {
+  photo.classList.add("animate__animated", "animate__flipInX");
+  photo.addEventListener("animationend", () => {
+    photo.classList.remove("animate__animated", "animate__flipInX");
+  });
+});
+
+rotate4.addEventListener("click", () => {
+  photo.classList.add("animate__animated", "animate__zoomInDown");
+  photo.addEventListener("animationend", () => {
+    photo.classList.remove("animate__animated", "animate__zoomInDown");
+  });
+});
+
+rotate5.addEventListener("click", () => {
+  photo.classList.add("animate__animated", "animate__slideInDown");
+  photo.addEventListener("animationend", () => {
+    photo.classList.remove("animate__animated", "animate__slideInDown");
+  });
+});
+
+// hidden p
+
+const p = document.querySelector(".main-container-child");
+
+document.querySelector("#hidden").onclick = () => {
+  p.classList.toggle("hidden");
+  console.log("some");
+};
